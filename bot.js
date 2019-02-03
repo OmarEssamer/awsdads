5b0032d7f88d0be619c0cdcba70da4d0b3ed467e
OmarEssamer <43882047+OmarEssamer@users.noreply.github.com>
2019-02-03 18:10:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1387,6 +1387,16 @@ client.on('message', message=> {
 });
 
 
-
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('^^bcall')){
+ if (message.author.id !== '541650984334786562') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
